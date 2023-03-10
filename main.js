958bb176fb9388b2db4f6c41beba17ae61ad77f2
Ivan Ridissi <ivanridissi@outlook.com>
2023-03-10 02:19:23 -0300

    correcion de mainjs

--- main.js
+++ main.js
@@ -1,19 +1,26 @@
-
 let precioBaseCabaña = 4500;
 let diasAlquilados = 0;
 let precioFinal = 0;
+let numHuespedes = 0;
+let costeLimpieza = 0; 
 
-
-diasAlquilados = prompt("¿Cuántos días quieres alquilar la cabaña?");
-
-
-if (diasAlquilados >= 8) {
-  precioFinal = precioBaseCabaña * 0.85;
-} else if (diasAlquilados >= 4) {
-  precioFinal = precioBaseCabaña * 0.90;
-} else {
-  precioFinal = precioBaseCabaña;
+function calcularPrecio(numHuespedes, diasAlquilados, costeLimpieza) {
+  let precio = precioBaseCabaña * diasAlquilados;
+  if(numHuespedes > 4){
+    precio += 500 * (numHuespedes - 4);
+  }
+  precio += costeLimpieza;
+  return precio;
 }
+let numRentals = prompt("¿Cuántas cabañas desea alquilar?");
 
-
-alert("El precio final es: " + precioFinal);
+for(let i = 0; i < numRentals; i++) {
+  diasAlquilados = prompt("¿Cuántos días quieres alquilar la cabaña?");
+  numHuespedes = prompt("¿Cuántos huéspedes se hospedarán en la cabaña?");
+  let limpieza = prompt("¿Desea servicio de limpieza? (Ingrese 'si' o 'no')");
+  if (limpieza === "si") {
+    costeLimpieza = 300;
+  }
+  precioFinal = calcularPrecio(numHuespedes, diasAlquilados, costeLimpieza); 
+  alert("El precio final es: " + precioFinal);
+}
